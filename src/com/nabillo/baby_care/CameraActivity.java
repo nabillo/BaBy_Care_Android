481021e3925b48129c1b91eeafcc9a0a71beedcc
nabillo <nabillo@users.noreply.github.com>
2014-04-11 11:25:54 +0100

    Update CameraActivity.java
    Add fling to start voice

--- src/com/nabillo/baby_care/CameraActivity.java
+++ src/com/nabillo/baby_care/CameraActivity.java
@@ -10,16 +10,41 @@ import android.widget.VideoView;
 public class CameraActivity extends Activity {
 
 	private SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+	private GestureDetectorCompat mDetector;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.Camera_View);
 		
+		mDetector = new GestureDetectorCompat(this, new MyGestureListener());
+		
+		// Implemente camera view with url of motion webserver
 		final VideoView cameraView = (VideoView) findViewById(R.id.cameraView);
 		videoView.setVideoPath(sharedPref.getString(SettingsActivity.KEY_PREF_CAM_URL, ""));
 		cameraView.start();
 	}
 
+	@Override 
+	public boolean onTouchEvent(MotionEvent event){ 
+		this.mDetector.onTouchEvent(event);
+		return super.onTouchEvent(event);
+	}
 
+	class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+		
+		@Override
+		public boolean onDown(MotionEvent event) { 
+			return true;
+		}
+		
+		@Override
+		public boolean onFling(MotionEvent event1, MotionEvent event2, 
+				float velocityX, float velocityY) {
+			// Start voice chat if scroll speed was high enough
+			if (velocityY > 10)
+				
+			return true;
+		}
+	}
 }
