11d4b02b0d4bc35e2c336b720543ba55beb879fc
nabillo <nabillo@users.noreply.github.com>
2014-04-11 17:51:24 +0100

    Update VoiceActivity.java
    Add Voice_Chat

--- src/com/nabillo/baby_care/VoiceActivity.java
+++ src/com/nabillo/baby_care/VoiceActivity.java
@@ -5,18 +5,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import com.nabillo.baby_care.Voice_Chat;
 
 public class VoiceActivity extends Activity {
 
 	private SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+	private voice_Chat = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.Voice_View);
-
-
+		voice_Chat = new Voice_Chat();
+		voice_Chat.voice_Start();
 	}
 
-
+	@Override
+	protected void onStop() {
+		super.onStop();
+		voice_Chat.voice_Stop();
+	}
+	
+	
 }
